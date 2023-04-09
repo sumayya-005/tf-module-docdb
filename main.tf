d4d25608a5ae7d076544f323b67960b2688314f2
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-09 12:20:21 +0530

    updating ./tf-module-docdb

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+resource "aws_docdb_subnet_group" "default" {
+  for_each   = var.docdb
+  name       = "${var.env}-${each.key}-roboshop-docdb"
+  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
+
+  tags = {
+    Name = "My docdb subnet group"
+  }
+}
+
+
 resource "aws_docdb_cluster" "docdb" {
   for_each                = var.docdb
   cluster_identifier      = "${var.env}-roboshop-docdb"
@@ -5,4 +16,5 @@ resource "aws_docdb_cluster" "docdb" {
   master_username         = "admin1"
   master_password         = "mustbeeightchars"
   skip_final_snapshot     = true
+  db_subnet_group_name    = "${var.env}-${each.key}-roboshop-docdb"
 }
