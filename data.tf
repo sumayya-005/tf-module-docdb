679d5f18b369b65837f088520fc38ee923d9c5bd
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-15 22:14:43 +0530

    updating ./tf-module-docdb

--- data.tf
+++ data.tf
@@ -1,3 +1,7 @@
 data "aws_secretsmanager_secret" "roboshop" {
-  name = "exmaple"
+  name = "roboshop/all-secrets"
+}
+
+data "aws_secretsmanager_secret_version" "roboshop" {
+  secret_id = data.aws_secretsmanager_secret.roboshop.id
 }
