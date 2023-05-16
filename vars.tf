b7ba0fa09b9d92df9c8d42322ac68decc5771fcc
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-16 15:18:00 +0530

    updating ./tf-module-docdb

--- vars.tf
+++ vars.tf
@@ -2,5 +2,5 @@ variable "engine" {}
 variable "subnets" {}
 variable "name" {}
 variable "env" {}
-variable "nodes" {}
-variable "skip_final_snapshot" {}
+#variable "nodes" {}
+#variable "skip_final_snapshot" {}
