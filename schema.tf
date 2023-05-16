38ddbf22a0ed34ab2a9e8e0b7654974ef74d6ba3
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-16 16:18:25 +0530

    updating ./tf-module-docdb

--- schema.tf
+++ schema.tf
@@ -1,3 +1,5 @@
-resource "null_resource" "load_schema" {
-  depends_on = [aws]
-}
+#resource "null_resource" "load_schema" {
+#  depends_on = [aws_docdb_cluster.docdb,aws_docdb_cluster_instance.cluster_instances]
+#
+#  provisioner "local-exec" {}
+#}
