305884955f42dcf2a0c864fadf3376e4c0c1cb78
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-16 16:40:20 +0530

    updating ./tf-module-docdb

--- schema.tf
+++ schema.tf
@@ -1,5 +1,17 @@
 #resource "null_resource" "load_schema" {
 #  depends_on = [aws_docdb_cluster.docdb,aws_docdb_cluster_instance.cluster_instances]
 #
-#  provisioner "local-exec" {}
+#  provisioner "local-exec" {
+#    command = <<EOF
+#
+#curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+#cd /tmp
+#unzip mongodb.zip
+#cd mongodb-main
+#curl -L -O https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
+#mongo --ssl --host dev-db1-roboshop-docdb.cluster-cjjzk2q3bnco.us-east-1.docdb.amazonaws.com:27017 --sslCAFile
+# rds-combined-ca-bundle.pem --username admin1 --password <insertYourPassword>
+#
+#EOF
+#  }
 #}
--- vars.tf
+++ vars.tf
@@ -2,8 +2,8 @@ variable "engine" {}
 variable "subnets" {}
 variable "name" {}
 variable "env" {}
-#variable "nodes" {}
-#variable "skip_final_snapshot" {}
+variable "nodes" {}
+variable "skip_final_snapshot" {}
 #variable "vpc_id" {}
 #variable "BASTION_NODE" {}
 #variable "vpc_cidr" {}
